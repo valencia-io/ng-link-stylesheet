a8e5a4b65691bf178b46887979fc76a7cbd18213
jupegarnica <macbookpro@MacBook-Pro-de-MacBook.local>
2015-10-31 18:18:11 +0100

    initial commit

---
+++ app/ngLinkStylesheet.js
@@ -0,0 +1,34 @@
+(function() {
+  'use strict';
+  angular
+    .module('ngLinkStylesheet', [])
+    .directive('ngLinkStylesheet', function() {
+      return {
+        restrict: 'A',
+        compile: function(element, attr) {
+          var _class = 'ngLinkStylesheet-' + attr.ngLinkStylesheet;
+          var $head;
+          // not in use now
+          //var _id = 'ngLinkStylesheet-' + Date().now();
+
+          //insert link if it's the first one or if allow-duplicates is an attribute
+          var _allowLink = attr.hasOwnProperty('ngLinkStylesheetAllowDuplicates') || document.getElementsByClassName(_class).length < 1;
+          if (_allowLink) {
+            $head = angular.element(document).find('head');
+            $head.append('<link class="' + _class + '" rel="stylesheet" href="' + attr.ngLinkStylesheet + '" type="text/css" media="screen">');
+
+          }
+
+          // destroy link if it was created but not destroy it if no-destroy is an attribute
+          if (_allowLink && attr.hasOwnProperty('ngLinkStylesheetNoDestroy')) {
+            return function(scope) {
+              scope.$on('$destroy', function() {
+                document.getElementsByClassName(_class).remove();
+              });
+            }
+          }
+
+        },
+      };
+    });
+}());
