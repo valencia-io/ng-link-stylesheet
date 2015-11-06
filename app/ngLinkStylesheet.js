0184e33568010fc1daa1fa6716ce7705b41e1040
jupegarnica <macbookpro@MacBook-Pro-de-MacBook.local>
2015-11-06 15:59:48 +0100

    auto mode working with baseurl

--- app/ngLinkStylesheet.js
+++ app/ngLinkStylesheet.js
@@ -7,7 +7,18 @@
             var head = document.getElementsByTagName('head')[0];
             var links = {}; // status of the links added to head
             function compile(element, attr) {
-
+                // Auto mode
+                if (!attr.ngLinkStylesheet || attr.ngLinkStylesheet.toLowerCase() === 'auto') {
+                    var baseUrl = '';
+                    if (attr.ngLinkStylesheetBaseUrl) {
+                        baseUrl = attr.ngLinkStylesheetBaseUrl;
+                    }
+                    if (attr.class) {
+                        attr.ngLinkStylesheet = baseUrl + attr.class.split(' ')[0] + '.css';
+                    } else {
+                        console.log('No class found for ng-link-stylesheet');
+                    }
+                }
 
                 var linkClass = 'ngLinkStylesheet-' + attr.ngLinkStylesheet;
                 var linkId = 'ngLinkStylesheet-' + Date.now();
@@ -21,7 +32,7 @@
                 linkTemplate.setAttribute("class", linkClass);
 
                 //insert link if is the first one
-                if (!links[linkClass]) {
+                if (!links[linkClass] || !links[linkClass].added ) {
                     head.appendChild(linkTemplate);
                     links[linkClass] = {added : true};
                 }
@@ -31,8 +42,8 @@
 
                     // destroy link if doesn't have no-destroy attribute,
                     // and has been appended, and the listener hasn't been created yet
-                    if (   links[linkClass]
-                        && links[linkClass].added
+                    if (    links[linkClass]
+                        &&  links[linkClass].added
                         && !links[linkClass].onDestoy
                         && !attr.hasOwnProperty('ngLinkStylesheetNoDestroy')) {
 
@@ -41,6 +52,7 @@
                             var element = document.getElementById(linkId);
                             if (element) {
                                 element.remove();
+                                links[linkClass] = {added : false, onDestoy: false};
                             }
                         });
                     }
